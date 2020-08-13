df60281e81f4daff213205bc135b6cf8e9d7dc8d
adirc93 <50356510+adirc93@users.noreply.github.com>
2020-08-13 03:11:17 +0300

    Adding a button and descriptions.

--- src/gui.erl
+++ src/gui.erl
@@ -10,8 +10,11 @@
 -author("asorg").
 -include_lib("wx/include/wx.hrl").
 
+-define(FONT_SIZE, 20).
+
 %% API
 -export([start/0,storage_button_click/2]).
+
 
 
 start() ->
@@ -25,7 +28,7 @@ start() ->
   ProxyButton = wxButton:new(Frame, ?wxID_ANY, [{label, "Proxy"}]),
   StorageButton = wxButton:new(Frame, ?wxID_ANY, [{label, "Storage"}]),
   %Set Font size
-  Font = wxFont:new(16, ?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?
+  Font = wxFont:new(?FONT_SIZE, ?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?
   wxFONTWEIGHT_BOLD),
   wxTextCtrl:setFont(ProxyButton, Font),
   wxTextCtrl:setFont(StorageButton, Font),
@@ -42,7 +45,8 @@ start() ->
   wxButton:connect(ProxyButton, command_button_clicked, [{callback, proxy_button_click}, {userData, Frame}]),
   wxButton:connect(StorageButton, command_button_clicked, [{callback, fun(Evt, Obj) -> storage_button_click(Evt, Obj)end}, {userData, Frame}]),
   %launch gui
-  wxFrame:show(Frame).
+  wxFrame:show(Frame),
+  timer:sleep(10000).
 
 proxy_button_click(#wx{ userData = #{userData := Frame}}, _Event)->
   W = wxWindow:new(Frame, ?wxID_ANY,"Storage mode"),
@@ -58,7 +62,7 @@ storage_button_click(Evt, _Obj) ->
   Capacity = wxTextCtrl:new(W, ?wxID_ANY, [{value, " "}]),
   %Button
   StartButton = wxButton:new(W, ?wxID_ANY, [{label, "Start"}]),
-  Font = wxFont:new(16, ?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?
+  Font = wxFont:new(?FONT_SIZE, ?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?
   wxFONTWEIGHT_BOLD),
   wxTextCtrl:setFont(StartButton, Font),
   %Set button background color
@@ -72,5 +76,45 @@ storage_button_click(Evt, _Obj) ->
   wxSizer:add(WindowSizer, Capacity, [{flag,?wxALIGN_CENTER bor ?wxALL},{border,5}]),
   wxSizer:add(WindowSizer, StartButton, [{flag,?wxALIGN_CENTER bor ?wxALL },{border,5}]),
   wxWindow:setSizer(W, WindowSizer),
+  wxButton:connect(StartButton, command_button_clicked, [{callback, fun(Evt, Obj) -> start_button_click(Evt, Obj)end}, {userData, Frame}]),
   wxFrame:show(W).
+
+%% This is an initialyze window, which will indicate to the user once finished.
+start_button_click(Evt, _Obj) ->
+  Frame = Evt#wx.userData,
+  W = wxFrame:new(Frame, ?wxID_ANY, "Adding to DB sa Storage node"),
+  %Text Boxes
+  Headline = wxStaticText:new(W, ?wxID_ANY, "Setting your storage as part of the database"),
+  %Button
+  CancelButton = wxButton:new(W, ?wxID_ANY, [{label, "Cancel"}]),
+  Font = wxFont:new(?FONT_SIZE, ?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?
+  wxFONTWEIGHT_BOLD),
+  wxTextCtrl:setFont(CancelButton, Font),
+  %Set button background color
+  wxButton:setBackgroundColour(CancelButton,?wxBLACK),
+
+  %Set component sizes and positions
+  WindowSizer = wxBoxSizer:new(?wxVERTICAL),
+  wxSizer:add(WindowSizer, Headline, [{flag,?wxALIGN_CENTER bor ?wxALL },{border,5}]),
+  wxSizer:add(WindowSizer, CancelButton, [{flag,?wxALIGN_CENTER bor ?wxALL },{border,5}]),
+  wxWindow:setSizer(W, WindowSizer),
+  wxButton:connect(CancelButton, command_button_clicked, [{callback, fun(Evt, Obj) -> terminate(Evt, Obj)end}, {userData, Frame}]),
+
+%  Widths_field = 10,
+%  StatusBar = wxStatusBar(),
+%  Parent = wxWindow(),
+%  create(StatusBar, Parent),
+%  setStatusWidths(StatusBar, Widths_field)
+   wxFrame:show(W).
+
+
+
+%% Here we will manage the main window which the storage node interact with
+
+
+%% terminate the window
+
+terminate(Evt, Obj) ->
+  wxFrame:destroy(),
+  ok.
 
